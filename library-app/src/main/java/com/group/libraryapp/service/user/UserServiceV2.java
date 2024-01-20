2ca77223ea2ac0f7ec70bc73469c066996a717ac
jae yeong <chujy1224@gmail.com>
2024-01-21 00:37:50 +0900

    - 사용자 서비스 계층에 트랜잭션 적용

--- library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,11 +6,13 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 public class UserServiceV2 {
 
     private final UserRepository userRepository;
@@ -19,6 +21,7 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
@@ -29,14 +32,15 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalAccessError::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name);
         if (user == null) {
